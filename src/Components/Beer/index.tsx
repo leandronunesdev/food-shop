af998b45128723a7af61d07c53afe045c5c56d9b
leandronunesdev <nunes.pessoal@gmail.com>
2022-08-12 14:52:21 -0300

    docs: updates readme

--- src/Components/Beer/index.tsx
+++ src/Components/Beer/index.tsx
@@ -1,35 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { InterfaceCerveja } from '../../types/InterfaceCerveja';
- 
-const Beer = () => {
-    
-    const[cervejas, setCervejas] = useState<InterfaceCerveja[]>([]) 
 
-    const getCervejas = () => {
-        axios.get('https://api.punkapi.com/v2/beers/?per_page=8')
-        .then(resposta => setCervejas(resposta.data))
-    }    
- 
-    return (
-        <div className="food-beer-list food-shop">        
-            <h1>Tipos de Cerveja</h1>
-            <button onClick={getCervejas}>Buscar Cerveja</button>
-            <div className="beers-list">
-                {
-                    cervejas !== undefined &&
-                    cervejas.map((item: InterfaceCerveja) => (
-                    <div key={item.id} className="beer">
-                        <img src={item.image_url} alt={item.name} />
-                        <h3>{item.name}</h3>
-                        <span>{item.tagline}</span>
-                        <small>{item.description}</small>
-                    </div>
-                    ))                    
-                }        
+const Beer = () => {
+  const [cervejas, setCervejas] = useState<InterfaceCerveja[]>([]);
+
+  const getCervejas = () => {
+    axios
+      .get('https://api.punkapi.com/v2/beers/?per_page=8')
+      .then((resposta) => setCervejas(resposta.data));
+  };
+
+  return (
+    <div className='food-beer-list food-shop'>
+      <h1>Beer Types</h1>
+      <button onClick={getCervejas}>Find Beers</button>
+      <div className='beers-list'>
+        {cervejas !== undefined &&
+          cervejas.map((item: InterfaceCerveja) => (
+            <div key={item.id} className='beer'>
+              <img src={item.image_url} alt={item.name} />
+              <h3>{item.name}</h3>
+              <span>{item.tagline}</span>
+              <small>{item.description}</small>
             </div>
-        </div>
-    );
-}
+          ))}
+      </div>
+    </div>
+  );
+};
 
 export default Beer;
--- src/Components/Food/index.tsx
+++ src/Components/Food/index.tsx
@@ -1,63 +1,74 @@
 import axios from 'axios';
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { InterfaceCategorias } from '../../types/InterfaceCategorias';
 import { InterfaceMenu } from '../../types/InterfaceMenu';
 
 const Foods = () => {
-
-  const [categorias, setCategorias] = useState<InterfaceCategorias[]>([])
-  const [categoryName, setCategoryName ] = useState<String>()
-  const [menu, setMenu] = useState<InterfaceMenu[]>([])
-  const [search, setSearch] = useState<String>() 
-  
-  useEffect(() => {
-    axios.get('https://www.themealdb.com/api/json/v1/1/categories.php')
-    .then(resposta => setCategorias(resposta.data.categories))
-  }, [])
+  const [categorias, setCategorias] = useState<InterfaceCategorias[]>([]);
+  const [categoryName, setCategoryName] = useState<String>();
+  const [menu, setMenu] = useState<InterfaceMenu[]>([]);
+  const [search, setSearch] = useState<String>();
 
   useEffect(() => {
-    if(categoryName !== null) {
-      axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoryName}`)
-      .then(resposta => setMenu(resposta.data.meals))
+    axios
+      .get('https://www.themealdb.com/api/json/v1/1/categories.php')
+      .then((resposta) => setCategorias(resposta.data.categories));
+  }, []);
+
+  useEffect(() => {
+    if (categoryName !== null) {
+      axios
+        .get(
+          `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoryName}`
+        )
+        .then((resposta) => setMenu(resposta.data.meals));
     }
-  },[categoryName]) 
+  }, [categoryName]);
 
   useEffect(() => {
-    if(search !== null) {
-      axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
-      .then(resposta => setMenu(resposta.data.meals))
+    if (search !== null) {
+      axios
+        .get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
+        .then((resposta) => setMenu(resposta.data.meals));
     }
-  },[search])
-  
+  }, [search]);
+
   return (
-    <div className="food-beer-list food-shop">
-      <h1>Tipos de pratos</h1>
+    <div className='food-beer-list food-shop'>
+      <h1>Dishes Types</h1>
       <p>
-        Selecione uma categoria ou digite a comida (em inglês):
-        <input type="text" placeholder="Digite a comida..." onChange={(event) => setSearch(event.target.value)}/>
+        Select the category or type the food:
+        <input
+          type='text'
+          placeholder='Search the food...'
+          onChange={(event) => setSearch(event.target.value)}
+        />
       </p>
       <ul>
-          { 
-            categorias !== null &&            
-            categorias.map((item: InterfaceCategorias) => (
-              <li key={item.idCategory} onClick={() => setCategoryName(item.strCategory)}>{item.strCategory}</li>
-            ))
-          }
+        {categorias !== null &&
+          categorias.map((item: InterfaceCategorias) => (
+            <li
+              key={item.idCategory}
+              onClick={() => setCategoryName(item.strCategory)}
+            >
+              {item.strCategory}
+            </li>
+          ))}
       </ul>
-      <h2>Tipo selecionado: <strong>{categoryName}</strong></h2>      
-      <div className="food-container">
-        {
-          menu !== null &&
+      <h2>
+        Type selected: <strong>{categoryName}</strong>
+      </h2>
+      <div className='food-container'>
+        {menu !== null &&
           menu.map((item: InterfaceMenu) => (
-            <div className="food-item" key={item.idMeal}>
-              <img src={item.strMealThumb} />
+            <div className='food-item' key={item.idMeal}>
+              <img src={item.strMealThumb} alt={item.strMeal} />
               <p>{item.strMeal}</p>
-          </div>
-          ))
-        }
+            </div>
+          ))}
       </div>
     </div>
   );
-}
+};
 
 export default Foods;
